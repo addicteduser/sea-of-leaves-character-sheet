23715c58006ecc040957e520f18fe56e8ca086a0
kristine1k <kristine1k@ai-pros.com>
2019-05-11 11:01:13 +0800

    Add boilerplate code

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,31 @@
 module Main exposing (main)
 
-import Html exposing (Html, text)
+import Html exposing (Html, div, text)
+import Touch
 
-main : Html msg
+
+type Msg
+    = Tapped
+
+
+update : Msg -> Int -> Int
+update Tapped count =
+    count + 1
+
+
+view : Int -> Html Msg
+view count =
+    div []
+        [ div [ Touch.onEnd (\event -> Tapped) ]
+            [ text "Hello from Elm!" ]
+        , text <| "Tap count: " ++ toString count
+        ]
+
+
+main : Program Never Int Msg
 main =
-  text "Hello from Elm!"
-
-
+    Html.beginnerProgram
+        { model = 0
+        , update = update
+        , view = view
+        }
